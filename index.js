912d172a772ef0a91c8995f8256bda79a9b50e44
Tanzi11 <tanzima.c@fellows.thedoschool.org>
2018-06-22 11:45:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
 const singers = ["John Lennon", "Ringo Star", "Paul McCartney", "Dizzy"];
 const instruments = ["guitar", "drums", "piano", "trumpet"];
+var theBeatlesPlay = function (musiciansArray, instrumentsArray) {
