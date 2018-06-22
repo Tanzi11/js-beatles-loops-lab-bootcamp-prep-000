2a5f812fa358eac1180aa1a7398c10f01a109ae7
Tanzi11 <tanzima.c@fellows.thedoschool.org>
2018-06-22 11:30:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,4 +6,5 @@ function theBeatlesPlay(players, instruments) {
   return allPlayers;
 }
 function johnLennonFacts() {
+  var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
   
